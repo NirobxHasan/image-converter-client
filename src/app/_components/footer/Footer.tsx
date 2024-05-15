d8da51abaf08ff00351d2e88b69e029549f2bb30
Nirob Hasan <thenirobhasan@gmail.com>
2024-05-15 17:58:55 +0600

    All added

---
+++ src/app/_components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div>
+            {/* <hr className=" border-gray-500 mx-20" /> */}
+            <div className="text-center text-white py-5">
+                <h1>
+                    Developed by <span>Nirob Hasan</span> ❤️
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
